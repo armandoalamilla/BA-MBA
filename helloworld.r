8a485aa4dab840b98c1c026128cf174049adedeb
Armando Aguiar <armandoalamilla@outlook.com>
2024-04-12 18:15:57 -0600

    initial commit
    test R file

---
+++ helloworld.r
@@ -0,0 +1,11 @@
+#Create dataframe
+libros=data.frame(
+   "id"=c(1,2,3,4),
+   "libros"=c("papalota", "dan brown", "la fortaleza", "libro playero")
+  )
+
+#Display the dataframe
+print(libros)
+
+trenmaya <- "se descarrillo"
+print(trenmaya)
