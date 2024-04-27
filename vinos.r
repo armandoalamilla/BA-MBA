10cc0404df66e8acc7e67864b1860177c7c336ab
Armando Aguiar <armandoalamilla@outlook.com>
2024-04-27 10:00:38 -0600

    vinos final?

--- vinos.r
+++ vinos.r
@@ -14,20 +14,30 @@ for (x in 1:10000) {
        Aleatorio2 <- randNumber()
 
        if (Aleatorio2 <= 0.6) { # escenario no hay botrytis
-          BeneficiosEsperar <- append(BeneficiosEsperar, 7200)          
+          BeneficiosEsperar <- append(BeneficiosEsperar, 24000)          
        } else { # escenario si hay botrytis
-          BeneficiosEsperar <- append(BeneficiosEsperar, 20640) #13440
+          BeneficiosEsperar <- append(BeneficiosEsperar, 67200) #13440
        }
  
     } else { #escenario donde no llega la tormenta
        Aleatorio3 <- randNumber()
 
-       if(Aleatorio3 <= 0.4) { #escenario contenido de azucar = 25%
-          BeneficiosEsperar <- append(BeneficiosEsperar, 29040) #8400
-       } else if (Aleatorio3 > 0.4 & Aleatorio3 <= 0.8) { # escenario azucar = 20%
-          BeneficiosEsperar <- append(BeneficiosEsperar, 44640) #7200
-       } else { #escenario contenido azucar < 19%
-          BeneficiosEsperar <- append(BeneficiosEsperar, 63240) #3000
+       if (Aleatorio3 <= 0.5) { #escenario contenido azucar 25%
+          Aleatorio4 <- randNumber()
+
+          if (Aleatorio4 <= 0.8) { #acidez > 0.7%
+             BeneficiosEsperar <- append(BeneficiosEsperar, 42000) #8400
+          } else { #acidez < 0.7%
+             BeneficiosEsperar <- append(BeneficiosEsperar, 30000) #1500
+          }
+       } else { #escenario azucar 20%
+          Aleatorio5 <- randNumber()
+
+          if (Aleatorio5 <= 0.8) { #acidez > 0.7%
+             BeneficiosEsperar <- append(BeneficiosEsperar, 36000) #7200
+          } else { #acidez < 0.7%
+             BeneficiosEsperar <- append(BeneficiosEsperar, 30000) #1500
+          }
        }
     }
 }
@@ -49,12 +59,15 @@ for (i in 1:10000) {
     }
 }
 
-print(cat("Num veces gana ahora: ", length(NumAhora), "\n"))
-print(cat("Num veces gana esperar: ", length(NumEspera), "\n"))
+print(cat("Num veces gana cosechar ahora: ", length(NumAhora), "\n"))
+print(cat("Num veces gana esperar cosecha: ", length(NumEspera), "\n"))
 print(cat("Num veces gana empate: ", length(NumEmpate), "\n"))
 
-print(length(NumAhora)/10000)
-print(length(NumEspera)/10000)
-print(length(NumEmpate)/10000)
+print("% que gana cosechar ahora: ")
+print((length(NumAhora)/10000)*100)
+print("% que gana esperar para cosechar: ")
+print((length(NumEspera)/10000)*100)
+print("% que gana empate: ")
+print((length(NumEmpate)/10000)*100)
 
 
